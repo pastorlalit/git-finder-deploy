e4c35edbbaa8a5133f62565e732dbbb01f0b39d6
pastorlalit <pastorlalit1989@gmail.com>
2019-11-05 23:50:08 +0530

    Updates

---
+++ precache-manifest.1c90cd1030b5e474b33cbfde073c47c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88036e0485662b401a9d7c7b23cdf73",
+    "url": "/git-finder-deploy/index.html"
+  },
+  {
+    "revision": "799d1c0dcc3719b2e6de",
+    "url": "/git-finder-deploy/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/git-finder-deploy/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "799d1c0dcc3719b2e6de",
+    "url": "/git-finder-deploy/static/js/main.a145e9e7.chunk.js"
+  },
+  {
+    "revision": "32695d7c2529c8bb32ef",
+    "url": "/git-finder-deploy/static/js/runtime~main.3137eaf7.js"
+  }
+]);
